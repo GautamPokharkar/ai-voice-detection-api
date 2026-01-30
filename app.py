0749258241c04d0cbc49776bf55c1ebca1d36c22
GautamPokharkar <gtpokharkar@gmail.com>
2026-01-30 16:38:26 +0530

    Integrate audio processing and AI voice detection logic

--- app.py
+++ app.py
@@ -1,1 +1,51 @@
+from fastapi import FastAPI, Header, HTTPException
+from pydantic import BaseModel
 
+
+from utils.audio_utils import load_audio_from_base64
+from model.detector import detect_ai_voice
+
+
+API_KEY = "sk_test_123456789"
+
+app = FastAPI()
+
+class VoiceRequest(BaseModel):
+    language: str
+    audioFormat: str
+    audioBase64: str
+
+
+@app.post("/api/voice-detection")
+def voice_detection(
+    request: VoiceRequest,
+    x_api_key: str = Header(None)
+):
+    
+    if x_api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="Invalid API key")
+
+    # 2️⃣ Audio format validation
+    if request.audioFormat.lower() != "mp3":
+        raise HTTPException(status_code=400, detail="Only mp3 format is supported")
+
+    # 3️⃣ Base64 MP3 → audio waveform (Person 2)
+    try:
+        y, sr = load_audio_from_base64(request.audioBase64)
+    except Exception as e:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Audio processing failed: {str(e)}"
+        )
+
+    
+    classification, confidence, explanation = detect_ai_voice(y, sr)
+
+    
+    return {
+        "status": "success",
+        "language": request.language,
+        "classification": classification,
+        "confidenceScore": confidence,
+        "explanation": explanation
+    }
